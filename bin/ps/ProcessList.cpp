1fd23644ff4373be390fafe822fc3292d2ec5aa3
semava <semavargas09@yahoo.com>
2022-10-20 19:34:29 -0700

    Started framework for renice

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -26,6 +26,7 @@ ProcessList::ProcessList(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
     parser().setDescription("Output system process list");
+    parser().registerFlag('l', "PriorityLevel", "Display priority level processes")
 }
 
 ProcessList::Result ProcessList::exec()
