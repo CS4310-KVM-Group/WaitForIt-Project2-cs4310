80b9c81e7dfed8a7cd45abb3585babebc9f466ef
Kenny Santanu <ksantanu@cpp.edu>
2022-10-23 00:09:50 -0700

    ps -l complete

--- bin/ps/ProcessList.cpp
+++ bin/ps/ProcessList.cpp
@@ -59,7 +59,7 @@ ProcessList::Result ProcessList::exec()
                 char line[128];
                 snprintf(line, sizeof(line),
                         "%3d %7d %9d %4d %5d %10s %32s\r\n",
-                        pid, info.kernelState.parent, 3, // <<<<< GET PRIORITY HERE
+                        pid, info.kernelState.parent, info.kernelState.priority,
                         0, 0, *info.textState, *info.command);
                 out << line;
             }
